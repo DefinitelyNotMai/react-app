a4abb4e0145c626d697253fe27703f0313a57e11
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2023-05-12 16:24:17 +0800

    Added managing state of selected list item.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,26 @@
-import { MouseEvent } from 'react';
+import { useState } from 'react';
 
 function ListGroup() {
   let items = ['Niway', 'Batwan', 'San Barbs', 'Pabie', 'Har', 'HiloHilo'];
-  items = [];
-
-  //Event Handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length == 0 && <p>No item found.</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
