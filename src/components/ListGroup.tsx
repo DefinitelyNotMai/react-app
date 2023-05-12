bf3e563422944223d54b7f1cc29151721d231106
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2023-05-12 13:51:13 +0800

    Conditional Rendering.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,11 @@
 function ListGroup() {
-  const items = ['Niway', 'Batwan', 'San Barbs', 'Pabie', 'Har', 'HiloHilo'];
+  let items = ['Niway', 'Batwan', 'San Barbs', 'Pabie', 'Har', 'HiloHilo'];
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length == 0 && <p>No item found.</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
