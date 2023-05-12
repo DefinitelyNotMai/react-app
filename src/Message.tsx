ea5819c8f244eda398177d671618409711bc5adb
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2023-05-12 12:58:26 +0800

    Created a Message module.

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+// PascalCasing - expected naming convention for functions.
+function Message() {
+  // This is JSX (JavaScript XML), not HTML.
+  // https://babeljs.io/repl
+  return <h1>Hello, User!</h1>;
+}
+
+export default Message;
