0bd8912119b518e8ddd17aae6c992a25cfb785a6
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2023-05-12 16:29:47 +0800

    Passing of Data via Props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from './components/ListGroup';
 
 function App() {
+  let items = ['Niway', 'Batwan', 'San Barbs', 'Pabie', 'Har', 'HiloHilo'];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
