fb080f059ffe84e4b8be34992cb2b3b2e68ba20c
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2023-05-12 17:07:32 +0800

    Added Button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from './components/Alert';
+import Button from './components/Button';
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <Alert>
         Hello, <span>Mai!</span>
       </Alert>
+      <Button color="primary" onClick={() => console.log('You clicked me :D')}>
+        Click me please.
+      </Button>
     </div>
   );
 }
